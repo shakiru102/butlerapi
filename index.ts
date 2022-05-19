518bca60590f16a51c0bf372559dd12072daac7c
shakiru102 <ashakiru53@gmail.com>
2022-05-19 07:08:22 +0100

    changedAuth

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api/v1', [AdminRoutes, UserRoutes])
+app.use('/api/v1', [AdminRoutes])
 app.get('/', (req: Request, res: Response) => res.status(200).send('Server is running'))
 
 server.listen(port, () => console.log('server runing on Port ' + port))
